a5f04176154dd5a6bbca896cbc7d79f1f77f17e0
Nischal Jadhav H <nischal.jadhav@gmail.com>
2022-10-21 23:32:00 +0530

    Time: 18 ms (97.16%), Space: 50.5 MB (94.34%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
@@ -3,9 +3,7 @@ class Solution {
         Map<Integer,Integer> map = new HashMap<>();
         for(int i = 0; i < nums.length;i++){
             if(map.containsKey(nums[i]) && Math.abs(i - map.get(nums[i])) <= k) return true;
-            else{
-                map.put(nums[i],i);
-            }
+            else map.put(nums[i],i);
         }
         return false;
     }
