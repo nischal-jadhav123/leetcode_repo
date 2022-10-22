e6826ebea32b5626012981cb7fcbd0e2b368b3cf
Nischal Jadhav H <nischal.jadhav@gmail.com>
2022-10-22 23:53:09 +0530

    Time: 3 ms (86.10%), Space: 55.6 MB (64.08%) - LeetHub

--- 1302-deepest-leaves-sum/1302-deepest-leaves-sum.java
+++ 1302-deepest-leaves-sum/1302-deepest-leaves-sum.java
@@ -14,20 +14,26 @@
  * }
  */
 class Solution {
+    private int maxLevel = 0;
+    private int sum = 0;
     public int deepestLeavesSum(TreeNode root) {
-        int sum = 0;
-        Queue<TreeNode> q = new LinkedList<>();
-        q.offer(root);
-        while(!q.isEmpty()){
-            int q_size = q.size();
-            sum = 0;
-            for(int i = 0; i < q_size;i++){
-                TreeNode t = q.poll();
-                sum += t.val;
-                if(t.left != null) q.offer(t.left);
-                if(t.right!= null) q.offer(t.right);
-            }
-        }
+        if(root == null) return 0;
+        calculateSumAtLevel(root,0);
         return sum;
+        
+    }
+    
+    private void calculateSumAtLevel(TreeNode root,int level){
+        
+       if(root == null) return;
+        if(level > maxLevel){
+            sum = 0;
+            maxLevel = level;
+        }
+        if(level == maxLevel){
+            sum = sum + root.val;
+        }
+        calculateSumAtLevel(root.left,level+1);
+        calculateSumAtLevel(root.right,level+1);
     }
 }
