7c518d0574cea12fafddedfc7636fc0ea0717004
Nischal Jadhav H <nischal.jadhav@gmail.com>
2022-10-24 01:32:38 +0530

    Time: 11 ms (39.99%), Space: 55.4 MB (14.82%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.java
+++ 0645-set-mismatch/0645-set-mismatch.java
@@ -9,17 +9,9 @@ class Solution {
                 arr[nums[i]] = nums[i];
             }
         }
-        // print(arr);
         for(int i = 1; i < arr.length;i++){
             if(arr[i] != i) res[1] = i;
         }
         return res;
     }
-    
-    // public void print(int[] arr){
-    //     for(int i = 0;i < arr.length;i++){
-    //         System.out.print(arr[i]);
-    //     }
-    //     System.out.println(" ");
-    // }
 }
