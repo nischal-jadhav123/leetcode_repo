694516fca1e7b98feb51848ef873c120f950bb1b
Nischal Jadhav H <nischal.jadhav@gmail.com>
2022-11-01 00:35:32 +0530

    Time: 1 ms (93.83%), Space: 45.3 MB (77.94%) - LeetHub

---
+++ 0766-toeplitz-matrix/0766-toeplitz-matrix.java
@@ -0,0 +1,10 @@
+class Solution {
+    public boolean isToeplitzMatrix(int[][] matrix) {
+        for(int i = 1; i < matrix.length;i++){
+            for(int j = 0; j < matrix[0].length - 1;j++){
+                if(matrix[i-1][j] != matrix[i][j+1]) return false;
+            }
+        }
+        return true;
+    }
+}
