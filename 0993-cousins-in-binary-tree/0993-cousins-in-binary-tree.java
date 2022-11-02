0483b6ce65a2fb7d917ea589d0d3bb8b7c943ded
Nischal Jadhav H <nischal.jadhav@gmail.com>
2022-11-02 23:48:41 +0530

    Time: 1 ms (64.60%), Space: 41.9 MB (50.62%) - LeetHub

--- 0993-cousins-in-binary-tree/0993-cousins-in-binary-tree.java
+++ 0993-cousins-in-binary-tree/0993-cousins-in-binary-tree.java
@@ -14,49 +14,22 @@
  * }
  */
 class Solution {
+    int d = -1;
+    TreeNode p = null;
     public boolean isCousins(TreeNode root, int x, int y) {
+        return helper(root,x,y,null,0);
+    }
+    private boolean helper(TreeNode root, int x, int y, TreeNode parent, int depth){
         if(root == null) return false;
-        Deque<List<TreeNode>> q = new LinkedList<>();
-        boolean res = false;
-        List<TreeNode> arr = new ArrayList<>();
-        arr.add(root);
-        arr.add(root);
-        q.offer(arr);
-        while(!q.isEmpty()){
-            int q_size = q.size();
-            int found = 0, foundParent = -1;
-            for(int i = 0; i < q_size;i++){
-                List<TreeNode> r = q.poll();
-                if(r.size() > 0){
-                    TreeNode curr = r.get(0);
-                    TreeNode parent = r.get(1);
-                if(x == curr.val && found == 0){
-                    found = 1;
-                    foundParent = parent.val;
-                }else if(x == curr.val && found == 1 && foundParent != parent.val){
-                    return true;
-                }else if(y == curr.val && found == 0){
-                    found = 1;
-                    foundParent = parent.val;
-                }else if(y == curr.val && found == 1 && foundParent != parent.val){
-                    return true;
-                }
-                    
-                    if(curr.left != null){
-                        List<TreeNode> arr1 = new ArrayList<>();
-                        arr1.add(curr.left);
-                        arr1.add(curr);
-                        q.offer(arr1);
-                    } 
-                    if(curr.right != null){
-                        List<TreeNode> arr2 = new ArrayList<>();
-                        arr2.add(curr.right);
-                        arr2.add(curr);
-                        q.offer(arr2);
-                    } 
-                } 
+        if(root.val == x || root.val == y){
+            if(d == -1){
+                d = depth;
+                p = parent;
+            }else{
+                if(p == parent || d != depth) return false;
+                else return true;
             }
         }
-        return false;
+        return helper(root.left,x,y,root,depth+1) || helper(root.right,x,y,root,depth+1);
     }
 }
